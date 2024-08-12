3ece27de776ca9ba2199fc2fee4950912040a1d5
liamarguedas <iliamftw2013@gmail.com>
2024-08-12 12:44:18 -0300

    add: manually commit

--- src/db_api.js
+++ src/db_api.js
@@ -4,10 +4,10 @@ export default class Database {
   constructor(db) {
     this.db = new sqlite3.Database(db);
   }
-
-  
-;
-    });
+  addProfile(profile) {
+    this.db.run("INSERT INTO Profiles (profile_name) VALUES ?", profile);
   }
-
+  deleteProfile(profile) {
+    this.db.run(`DELETE FROM Profiles WHERE profile_name = ?`, profile);
+  }
 }
